1f08db54be98e834ec29e8e599351bb765297c18
brunalopesd <bruna.lopes@resultadosdigitais.com.br>
2020-01-07 15:39:56 -0300

    add a integration test simple  example

---
+++ integration_test_example.rb
@@ -0,0 +1,15 @@
+
+require "rails_helper"
+
+feature "user creates person" do
+  scenario "with valid data" do
+    visit new_person_url
+
+    fill_in "Name",	with: "Bruna" 
+
+    click_button "Submmit"
+
+    expect(page).to have_content ("Thanks") 
+
+  end
+end
