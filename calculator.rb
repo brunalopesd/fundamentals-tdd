80de2601a2c68e64c1c003766a160239cbd69dcb
Bruna Lopes <48215727+brunalopesd@users.noreply.github.com>
2022-07-19 09:48:18 -0300

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -4,8 +4,8 @@ require "rspec/autorun"
 # calc.add(5, 10) # => 15
 
 class Calculator
-    def add(number_a, number_b)
-        number_a + number_b
+    def add(a, b)
+        a + b
     end
     
     def subtract(a, b)
